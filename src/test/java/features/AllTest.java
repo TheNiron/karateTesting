2cd40e14a9f3e4dbfd9a766fdc92c5c7dbf85dfe
TheRasanjana <snironr@gmail.com>
2019-09-14 09:30:17 +0530

    adding pom changes

--- src/test/java/features/AllTest.java
+++
@@ -1,9 +0,0 @@
-package features;
-
-import com.intuit.karate.KarateOptions;
-import com.intuit.karate.junit4.Karate;
-import org.junit.runner.RunWith;
-
-@RunWith(Karate.class)
-@KarateOptions(tags = "~@ignore")
-public class AllTest { }
---
+++ src/test/java/features/AllTester.java
@@ -0,0 +1,36 @@
+package features;
+
+import com.intuit.karate.KarateOptions;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
+
+@KarateOptions(tags = {"~@ignore"}) // important: do not use @RunWith(Karate.class) !
+public class AllTest {
+    @Test
+    public void testParallel() {
+        System.setProperty("mock.env", "karateTesting"); // ensure reset if other tests (e.g. mock) had set env in CI
+        Results results = Runner.parallel(getClass(), 5);
+        AllTest.generateReport(results.getReportDir());
+        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
+    }
+
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target/cucumber-html-reports"), "karateTesting");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+    }
+}
